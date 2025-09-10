a77c16b4ca1e65dcd8cec6bf9c3003e2cdee30e9
ezpedersen <erikzhangpedersen@gmail.com>
2025-09-10 17:54:18 -0400

    name update + sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://cornellgenai.com',  // replace with your actual site
+  generateRobotsTxt: true,             // generates robots.txt file
+}
+
